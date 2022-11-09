e8881100161901479c5fed11a169e84f4be92c59
Joti <tanjilajoti@gmail.com>
2022-11-09 14:07:23 +0600

    basic mongo setup

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-// const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-// require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,14 +9,21 @@ const port = process.env.PORT || 5000;
 // middle wares
 app.use(cors());
 app.use(express.json());
-
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ijmmtxg.mongodb.net/?retryWrites=true&w=majority`;
+// console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
 // run().catch(err => console.error(err));
 
 
 app.get('/', (req, res) => {
-    res.send('genius car server is running')
+    res.send('delivery services server is running')
 })
 
 app.listen(port, () => {
-    console.log(`Genius Car server running on ${port}`);
+    console.log(`delivery services server running on ${port}`);
 })
