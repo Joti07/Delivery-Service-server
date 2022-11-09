c666e42822790de8d258b457d8f9260dbc5daf89
Joti <tanjilajoti@gmail.com>
2022-11-09 12:23:08 +0600

    create init server

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+// const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+// require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middle wares
+app.use(cors());
+app.use(express.json());
+
+// run().catch(err => console.error(err));
+
+
+app.get('/', (req, res) => {
+    res.send('genius car server is running')
+})
+
+app.listen(port, () => {
+    console.log(`Genius Car server running on ${port}`);
+})
